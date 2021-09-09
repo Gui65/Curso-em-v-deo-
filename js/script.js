64b4d999321430571a1a6f50841fa6960d9d50cf
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-09-09 08:45:23 -0300

    Finalizando

--- js/script.js
+++ js/script.js
@@ -6,5 +6,17 @@ function carregar() {
     var minuto = data.getMinutes()
     var segundo = data.getSeconds()
     msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}`
-
+    if(hora >=0 && hora < 12){
+        //BOM DIA
+        img.src = './img/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >=12 && hora < 18){
+        //BOA TARDE
+        img.src = './img/tarde.jpg'
+        document.body.style.background = '#b9846f'
+    }else{
+        //BOA NOITE
+        img.src = './img/noite.jpg'
+        document.body.style.background = '#515154'
+    }
 }
